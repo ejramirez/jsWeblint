73126703083bd6485f2be47ff919836d36777497
ejramirez <phobus309@gmail.com>
2017-01-25 07:30:07 -0600

    Add Plugin

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# jsWeblint
-Senior project.
+# eslint-plugin-01
+
+Lints for potential sql injection flaws
+
+## Installation
+
+You'll first need to install [ESLint](http://eslint.org):
+
+```
+$ npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-01`:
+
+```
+$ npm install eslint-plugin-01 --save-dev
+```
+
+**Note:** If you installed ESLint globally (using the `-g` flag) then you must also install `eslint-plugin-01` globally.
+
+## Usage
+
+Add `01` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+
+```json
+{
+    "plugins": [
+        "01"
+    ]
+}
+```
+
+
+Then configure the rules you want to use under the rules section.
+
+```json
+{
+    "rules": {
+        "01/rule-name": 2
+    }
+}
+```
+
+## Supported Rules
+
+* Fill in provided rules here
+
+
+
+
+
---
+++ tests/lib/rules/sqlinject.js
@@ -0,0 +1,54 @@
+/**
+ * @fileoverview prevents sql injection flaws
+ * @author Eric
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/sqlinject"),
+
+    RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester();
+ruleTester.run("sqlinject", rule, {
+
+    valid: [
+
+        // give me some code that won't trigger a warning
+        "var i = strip_tags('Some Text','');",
+        "var i = strip_tags('<script>malicious code</script>','');",
+        "var i = strip_tags('<h1><a></a></h1>','<h1>');"
+    ],
+
+    invalid: [
+        {
+            code: "var i = \"<script></script>\";",
+            errors: [{
+                message: "unsanitized variable detected",
+                type: "VariableDeclaration"
+            }]
+        },
+        {
+            code: "var i = 'Some Text'",
+            errors: [{
+                message: "unsanitized variable detected",
+                type: "VariableDeclaration"
+            }]
+        },
+        {
+            code: "var i = alert('alarming')",
+            errors: [{
+                message: "no strip_tags() call on string",
+                type: "VariableDeclaration"
+            }]
+        }
+    ]
+});
