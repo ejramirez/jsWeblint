d8e841ae135500b1435494d8302fd366393e6775
ejramirez <phobus309@gmail.com>
2017-01-26 11:37:30 -0600

    Quick Update

--- tests/lib/rules/sqlinject.js
+++ tests/lib/rules/sqlinject.js
@@ -23,23 +23,25 @@ ruleTester.run("sqlinject", rule, {
     valid: [
 
         "var i = 'hi'",
-        "var x = 1"
+        "var x = 1",
+        "var i = 'SELECT * FROM id=?;'",
+        "var i = 'SELECT * FROM table WHERE id=1 and id2=?';" // <--- This Shouldn't pass.
         
     ],
 
     invalid: [
        
         {
-            code: "var i = 'SELECT * FROM;'",
+            code: "var i = 'SELECT * FROM table WHERE id=1';",
             errors: [{
-                message: "sql string detected.",
+                message: "sql string not parameterized.",
                 type: "VariableDeclaration"
             }]
         },
         {
-            code: "var i = 'INSERT * FROM;'",
+            code: "var i = 'INSERT * FROM id=1';",
             errors: [{
-                message: "sql string detected.",
+                message: "sql string not parameterized.",
                 type: "VariableDeclaration"
             }]
         }
